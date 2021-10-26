34814d2c380f26843b8223f5ab73184e3f2744c4
jcumgedu <jrosalesi@miumg.edu.gt>
2021-10-26 15:47:48 -0600

    cambio1

--- calculadora.php
+++ calculadora.php
@@ -5,7 +5,7 @@
 		<title>Operadores Aritm&eacute;meticos</title>
 	</head>
 	<body>
-		<h1>Calculadora</h1>
+		<h1>bienvenido a calculadora php</h1>
 		<form action="resultado.php" method="post">
 			<select name="operador">
 				<option value="suma">Suma</option>
